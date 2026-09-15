cdd13b4ef1476e263d7e6078c6ff99e7709f0bc6
Edgar <bleepbloopproducts@gmail.com>
2026-09-15 11:08:32 +0000

    fix GitHub Pages route handling

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/TableOrders-Landing">
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
